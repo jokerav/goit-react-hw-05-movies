cd8e4e1ade3e9a094902d4789003fa1890c8afb5
jokerav <50701756+jokerav@users.noreply.github.com>
2022-06-21 23:08:48 +0300

    Update Home.jsx

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,7 +1,9 @@
 import axios from 'axios';
 import { useState } from 'react';
+import { Link, useMatch } from 'react-router-dom';
 const Home = () => {
   const [trandingMovie, setTrandingMovie] = useState([]);
+  const { url } = useMatch;
   axios
     .get(
       'https://api.themoviedb.org/3/trending/movie/day?api_key=9e5cf4f45ae60b7760108794dc459813'
@@ -13,7 +15,9 @@ const Home = () => {
   return (
     <ul>
       {trandingMovie.map(movie => (
-        <li>{movie.original_title}</li>
+        <li key={movie.id}>
+          <Link to={`${url}/${movie.id}`}> {movie.original_title}</Link>
+        </li>
       ))}
     </ul>
   );
