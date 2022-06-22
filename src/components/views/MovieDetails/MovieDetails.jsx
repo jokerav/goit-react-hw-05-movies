1b4ee786935957d3d164e08c5a32c693181a7c30
jokerav <50701756+jokerav@users.noreply.github.com>
2022-06-22 13:29:44 +0300

    MovieDetails

--- src/components/views/MovieDetails/MovieDetails.jsx
+++ src/components/views/MovieDetails/MovieDetails.jsx
@@ -1,4 +1,20 @@
-const MovieDetails = movie => {
-  return <p>its a movie</p>;
+import axios from 'axios';
+import { useEffect } from 'react';
+import { useState } from 'react';
+import { useParams } from 'react-router-dom';
+
+const MovieDetails = () => {
+  const { movieId } = useParams();
+  const [movie, setMovie] = useState({});
+  useEffect(() => getMovie());
+  const getMovie = () => {
+    axios
+      .get(
+        `https://api.themoviedb.org/3/movie/${movieId}?api_key=9e5cf4f45ae60b7760108794dc459813&language=en-US`
+      )
+      .then(response => setMovie(response.data));
+  };
+  const { title } = movie;
+  return <h1>{title}</h1>;
 };
 export default MovieDetails;
