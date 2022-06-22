0ee3d2a010352591bbd67d7d065870c1573673c7
jokerav <50701756+jokerav@users.noreply.github.com>
2022-06-22 12:13:15 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,16 +5,7 @@ import MovieDetails from './views/MovieDetails/MovieDetails';
 
 export const App = () => {
   return (
-    <div
-    // style={{
-    //   height: '100vh',
-    //   display: 'flex',
-    //   justifyContent: 'center',
-    //   alignItems: 'center',
-    //   fontSize: 40,
-    //   color: '#010101',
-    // }}
-    >
+    <div>
       <Navigation />
       <Routes>
         <Route exact path="/" element={<Home />}></Route>
