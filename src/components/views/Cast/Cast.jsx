2b0bcc4d3ccdd2cd9e2d1d42963bac0ba8b3ff12
jokerav <50701756+jokerav@users.noreply.github.com>
2022-06-23 22:30:52 +0300

    Update Cast.jsx

--- src/components/views/Cast/Cast.jsx
+++ src/components/views/Cast/Cast.jsx
@@ -1,4 +1,25 @@
+import { useParams } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 const Cast = () => {
-  return <h2>Its a cast</h2>;
+  const { movieId } = useParams();
+  const [casts, setCast] = useState([]);
+  const getMovie = movieId => {
+    axios
+      .get(
+        `
+https://api.themoviedb.org/3/movie/${movieId}/credits?api_key=9e5cf4f45ae60b7760108794dc459813&language=en-US`
+      )
+      .then(response => setCast(response.data.cast));
+  };
+  useEffect(() => getMovie(movieId), [movieId]);
+  console.log(casts);
+  return (
+    <ul>
+      {casts.map(({ cast_id, original_name }) => (
+        <li key={cast_id}>{original_name}</li>
+      ))}
+    </ul>
+  );
 };
 export default Cast;
