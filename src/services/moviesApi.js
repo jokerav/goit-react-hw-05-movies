0f133910a6b33e053b0dfdbccffecfbf8f5578c7
jokerav <50701756+jokerav@users.noreply.github.com>
2022-06-26 12:19:58 +0300

    getReviews

--- src/services/moviesApi.js
+++ src/services/moviesApi.js
@@ -30,5 +30,12 @@ const getMovieDetails = async movieId => {
   );
   return response.data;
 };
-export { getCast, getMovie, getTrandingMovie, getMovieDetails };
+const getReviews = async movieId => {
+  const response = await axios.get(
+    `
+https://api.themoviedb.org/3/movie/${movieId}/reviews?api_key=9e5cf4f45ae60b7760108794dc459813&language=en-US&page=1`
+  );
+  return response.data.results;
+};
+export { getCast, getMovie, getTrandingMovie, getMovieDetails, getReviews };
 //
--- src/views/Reviews/Reviews.jsx
+++ src/views/Reviews/Reviews.jsx
@@ -1,18 +1,21 @@
 import { useParams } from 'react-router-dom';
 import { useState, useEffect } from 'react';
-import axios from 'axios';
+import { getReviews } from 'services/moviesApi';
+
 const Reviews = () => {
   const { movieId } = useParams();
   const [reviews, setReviews] = useState([]);
-  const getMovie = movieId => {
-    axios
-      .get(
-        `
-https://api.themoviedb.org/3/movie/${movieId}/reviews?api_key=9e5cf4f45ae60b7760108794dc459813&language=en-US&page=1`
-      )
-      .then(response => setReviews(response.data.results));
-  };
-  useEffect(() => getMovie(movieId), [movieId]);
+  //   const getMovie = movieId => {
+  //     axios
+  //       .get(
+  //         `
+  // https://api.themoviedb.org/3/movie/${movieId}/reviews?api_key=9e5cf4f45ae60b7760108794dc459813&language=en-US&page=1`
+  //       )
+  //       .then(response => setReviews(response.data.results));
+  //   };
+  useEffect(() => {
+    getReviews(movieId).then(result => setReviews(result));
+  }, [movieId]);
 
   return (
     <ul>
