c8afc98fab25f438cf8b2140c4ba739d17ad3aca
jokerav <50701756+jokerav@users.noreply.github.com>
2022-06-26 12:21:08 +0300

    Update MovieDetails.jsx

--- src/views/MovieDetails/MovieDetails.jsx
+++ src/views/MovieDetails/MovieDetails.jsx
@@ -7,15 +7,7 @@ const MovieDetails = () => {
   const [movie, setMovie] = useState({});
   const location = useLocation();
   const backLinkHref = location.state?.from ?? '/';
-  // const getMovie = movieId => {
-  //   axios
-  //     .get(
-  //       `https://api.themoviedb.org/3/movie/${movieId}?api_key=9e5cf4f45ae60b7760108794dc459813&language=en-US`
-  //     )
-  //     .then(response => {
-  //       setMovie(response.data);
-  //     });
-  // };
+
   useEffect(() => {
     getMovieDetails(movieId).then(movie => setMovie(movie));
   }, [movieId]);
